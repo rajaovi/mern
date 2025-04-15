c45d8bef8c1f63debed43a4bbfaed825c13da666
Raja <orajacse@gmail.com>
2025-04-15 16:01:36 +0530

    MERN - BE - FORGOT PASSWORD

--- mern-tailwind/backend/controller/authController.js
+++ mern-tailwind/backend/controller/authController.js
@@ -1,7 +1,13 @@
 import User from "../models/userModal.js";
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+
 import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail } from "../mailTrap/email.js";
+import {
+  sendVerificationEmail,
+  sendWelcomeEmail,
+  sendPasswordResetEmail,
+} from "../mailTrap/email.js";
 
 const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -124,9 +130,45 @@ const login = async (req, res) => {
   }
 };
 
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Generate reset token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+    await user.save();
+
+    // send email
+    await sendPasswordResetEmail(
+      user.email,
+      `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset link sent to your email",
+    });
+  } catch (error) {
+    console.log("Error in forgotPassword ", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 const logout = async (req, res) => {
   res.clearCookie("token");
   res.status(200).json({ success: true, message: "Logged out successfully" });
 };
 
-export { signup, login, logout, verifyEmail };
+export { signup, login, logout, verifyEmail, forgotPassword };
