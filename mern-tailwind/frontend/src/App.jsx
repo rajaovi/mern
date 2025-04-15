7ebc1015f85559993ea641394f7740ca460bac5c
Raja <orajacse@gmail.com>
2025-04-15 21:22:40 +0530

    MERN - FE - Adding Taiilwind css

--- mern-tailwind/frontend/src/App.jsx
+++ mern-tailwind/frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import FloatingShape from "./components/FloatingShapes";
 
 const App = () => {
   return (
-    <h1 className='text-green-500 text-5xl' >Hello</h1>
-  )
+    <section className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+    </section>
+  );
 }
 
 export default App
